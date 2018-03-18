03029b440c4da6c1d7295bfb0a4bf6feb42c64d0
barboss <barboss@barbosss-iMac.local>
2018-03-18 22:14:27 +0200

    1

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,13 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +15,30 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('--- constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    console.log('--- onChanges called');
+    console.log('changes', changes);
+    // this.name = changes.currentValue.name;
+  }
 
   ngOnInit() {
+    console.log('--- ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('--- ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('--- AfterContentInit called');
   }
 
 }
